1da9d9b8430d0eee37c21d3a95f8a2af5c994103
Antoine Beroud <toinan.beroud@gmail.com>
2026-01-26 10:23:34 +0100

    maj min

---
+++ asf_0702.R
@@ -0,0 +1,17 @@
+
+#                                               ASF - SCRIPT DE LA PLANCHE 0702
+#                                                       PAUVRETES ET INEGALITES
+#
+#                                                                antoine beroud
+#                                                           antonine ribardiere
+
+library(sf)
+library(asf)
+library(mapsf)
+
+
+# IMPORT DU FICHIER -----------------------------------------------------------
+data <- st_read("input/asf_0702/pauvrete_fideli_2022.gpkg")
+
+
+
