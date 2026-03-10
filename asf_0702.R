3a345cf8116abf8fdebb4a0a377c21ef8f649aed
Antoine Beroud <toinan.beroud@gmail.com>
2026-03-10 14:42:34 +0100

    maj min

--- asf_0702.R
+++ asf_0702.R
@@ -24,30 +24,36 @@ geom <- mar$geom
 
 fond <- asf_fond(geom, tabl, by = "IRISF_CODE", maille = "IRISrD_CODE")
 fond <- asf_drom(fond)
+
 fond_05 <- asf_simplify(fond, keep = 0.5)
 fond_09 <- asf_simplify(fond, keep = 0.9)
 
-z <- asf_zoom(f = fond_09, 
-              places = c("5", "4", "Dijon", "Reims", "Rouen"), 
-              # coords = c(2.584, 49.207), labels = "Senlis", 
-              nb_cols = 7, r = 20000)
+z <- asf_zoom(f = fond_09,
+              places = c("5", "4", "Dijon", "Reims", "Rouen"),
+              nb_cols = 7)
 
 
 x <- st_read("input/asf_0702/pauvre.gpkg")
 x <- st_drop_geometry(x)
+c <- asf_fondata(f = fond_01, d = x, by = "IRISrD_CODE")
 
-c <- asf_fondata(f = fond_05, z = z[[1]], d = x, by = "IRISrD_CODE")
 
-sum(nchar(c$tx_menpauvre), na.rm = TRUE) - sum(nchar(x$tx_menpauvre), na.rm = TRUE)
+# c <- st_transform(c, crs = 4326)
+# 
+# sum(nchar(c$tx_menpauvre), na.rm = TRUE) - sum(nchar(x$tx_menpauvre), na.rm = TRUE)
+# st_write(c, "planche_0702_01.geojson")
+# 
+# mf_map(c)
+
 
 
 # CARTO ----
-pal <- c("#e94256", 
-         "#f08590", 
-         "#f8c7c8", 
-         "#dadada", 
-         "#9d9d9c", 
-         "#575756")
+pal <- c("#ea5289", 
+         "#f087b0", 
+         "#f8c8d8", 
+         "#dae0e3", 
+         "#b7c6cf", 
+         "#95a6b1")
 
 mf_map(c, var = "tx_menpauvre", type = "choro", 
        breaks = quantile(c$tx_menpauvre, probs = c(0, 0.05, 0.25, 0.5, 0.75, 0.95, 1), na.rm = TRUE),
@@ -56,12 +62,12 @@ mf_label(z[[2]], var = "label")
 
 
 
-pal <- c("#009c79", 
-         "#7dc4a3", 
-         "#c0e0cd", 
-         "#dadada", 
-         "#9d9d9c", 
-         "#575756")
+pal <- c("#f59c00", 
+         "#ffce44", 
+         "#ffe596", 
+         "#dae0e3", 
+         "#b7c6cf", 
+         "#95a6b1")
 
 mf_map(c, var = "interd", type = "choro", 
        breaks = quantile(c$interd, probs = c(0, 0.05, 0.25, 0.5, 0.75, 0.95, 1), na.rm = TRUE),
@@ -70,27 +76,20 @@ mf_label(z[[2]], var = "label")
 
 
 pal <- c(
-  "low_low" =       "#e0d6c4", 
-  "middle_low" =    "#7dc4a3", 
-  "high_low" =      "#009c79", 
+  "low_low" =       "#feebdc", 
+  "middle_low" =    "#ffe596", 
+  "high_low" =      "#ffce44", 
   
-  "low_middle" =    "#f08590", 
-  "middle_middle" = "#aa7d77", 
-  "high_middle" =   "#006757", 
+  "low_middle" =    "#f8c8d8", 
+  "middle_middle" = "#f8b999", 
+  "high_middle" =   "#f07f3c", 
   
-  "low_high" =      "#e94256", 
-  "middle_high" =   "#95254c", 
-  "high_high" =     "#3d3b42"
+  "low_high" =      "#f087b0", 
+  "middle_high" =   "#e84250", 
+  "high_high" =     "#a6393a"
 )
 
 mf_map(c, "class", type = "typo", pal = pal, border = NA, 
        val_order = c("low_low", "middle_low", "high_low",
                      "low_middle", "middle_middle", "high_middle",
                      "low_high", "middle_high", "high_high"))
-
-
-na <- c[is.na(c$tx_menpauvre) | c$tx_menpauvre == 0.0, ]
-mf_map(na)
-
-na <- c[is.na(c$interd), ]
-mf_map(na)
